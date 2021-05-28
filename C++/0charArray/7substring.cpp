326bf7cf2387b0f0a3bf7fb22bebd1ed07e5397f
Soikat Rahman <hello.dev.sr@gmail.com>
2021-05-29 02:03:58 +0600

    C++: Substring from char array

--- C++/0charArray/7substring.cpp
+++ C++/0charArray/7substring.cpp
@@ -7,8 +7,8 @@ using namespace std;
 void printSubString(char *a) {
     for (int i = 0; a[i] != '\0'; i++) {
         for (int j = i; a[j] != '\0'; j++) {
-            cout << "{" << i << "," << j << "}"
-                 << " ";
+            cout << "printing in range :{" << i << "," << j << "}"
+                 << " -> ";
             //Print all the chars from i to j
             for (int k = i; k <= j; k++) {
                 cout << a[k];
