0dabda55bd1799e0a2feeeb962cf2f2d904dc32b
Soikat Rahman <hello.dev.sr@gmail.com>
2021-05-29 06:26:47 +0600

    C++ : printing color message

---
+++ C++/0string/00_ColorMsgPrinting.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+
+    cout << setfill('*') << setw(80) << "*" << endl;
+
+    cout << endl;
+    cout << "\x1B[32m*" << setfill('*') << setw(80) << "\x1B[32m*" << endl;
+
+    cout << endl;
+    cout << "\x1B[32m*" << setfill('*') << setw(30) << "\x1B[32m*";
+    cout << "\x1B[32mCOLOR";
+    cout << setfill('*') << setw(30) << "\x1B[32m*" << endl;
+    cout << endl;
+
+    cout << "\x1B[31mRED" << endl;
+    printf("\x1B[32mGREEN\n");
+    printf("\x1B[33mBLUE\n");
+    printf("\x1B[34mPURPLE\n");
+    printf("\x1B[35mTexting\n");
+    printf("\x1B[36mTexting\n");
+    printf("\x1B[36mTexting\n");
+    printf("\x1B[36mTexting\n");
+    printf("\x1B[37mTexting\n");
+    printf("\x1B[93mTexting\n");
+    printf("\033[3;42;30mTexting\n");
+    printf("\033[3;43;30mTexting\n");
+    printf("\033[3;44;30mTexting\n");
+    printf("\033[3;104;30mTexting\n");
+    printf("\033[3;100;30mTexting\n");
+
+    printf("\033[3;47;35mTexting\n");
+    printf("\033[2;47;35mTexting\n");
+    printf("\033[1;47;35mTexting\n");
+
+    return 0;
+}
