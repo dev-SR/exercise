e7689a59f329630e9b2572f118a8cb8114616c11
Soikat Rahman <hello.dev.sr@gmail.com>
2021-05-30 18:30:20 +0600

    C++: Dynamic Memory Allocation v1.1

--- C++/2_dynamic_memory_alloc/00dynamic_memory_in_cpp.cpp
+++ C++/2_dynamic_memory_alloc/00dynamic_memory_in_cpp.cpp
@@ -8,10 +8,12 @@ using namespace std;
 #define CYN "\e[0;96m"
 #define reset "\e[0m"
 
+int *getArray(int n);
 void display(int *p, int n) {
     for (int i = 0; i < n; i++) {
-        cout << p[i];
+        cout << p[i] << " ";
     }
+    cout << endl;
 }
 
 int main() {
@@ -31,5 +33,32 @@ int main() {
 
     //deallocate memory
     delete[] p;
+
+    //Example: DMA and returing pointer from functions:
+    cout << BLU << "EX:" << reset << endl;
+    int *ptr = getArray(10);
+    display(ptr, 10);
+    delete[] ptr;
     return 0;
+}
+
+int *getArray(int n) {
+    int *p = new int[n];
+
+    srand(time(NULL)); //time_t t = time(NULL);
+    /**
+    X://? void srand(unsigned seed) : 
+     * Seeds the pseudo - random number generator used by 
+     * rand() with the value seed.
+
+    The srand(x) function sets the seed of the random number generator algorithm used by the function rand( ).  A seed value of 1 is the default setting yielding the same sequence of values as if srand(x) were not used.  Any other value for the seed produces a different sequence.
+    
+    X://? srand(time(NULL));
+    makes use of the computer's internal clock to control the choice of the seed.  Since time is continually changing, the seed is forever changing.  
+     * https://mathbits.com/MathBits/CompSci/LibraryFunc/rand.htm
+    */
+    for (int i = 0; i < n; i++) {
+        p[i] = rand() % 100;
+    }
+    return p;
 }
