aa47433fbbc84461be7774da40f155cd1c549163
Soikat Rahman <hello.dev.sr@gmail.com>
2021-05-30 21:42:54 +0600

    C++ : DMA-> 2D array v0

---
+++ C++/2_dynamic_memory_alloc/02_2DDynamicArray.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define RED "\e[0;91m"
+#define GRN "\e[0;92m"
+#define YEL "\e[0;93m"
+#define BLU "\e[0;94m"
+#define MAG "\e[0;95m"
+#define CYN "\e[0;96m"
+#define reset "\e[0m"
+
+int main() {
+    /**
+       int **p = new int*[2]; <- array of Integer Pointers
+             ┃                           
+             ▼
+       ┏━━━━━━┓      ┏━━━━━━━━━━━━━━━┓
+       ┃ p[0] ┃----> ┃ new int[n];   ┃
+       ┗━━━━━━┛      ┗━━━━━━━━━━━━━━━┛
+       ┏━━━━━━┓      ┏━━━━━━━━━━━━━━━┓
+       ┃ p[1] ┃----> ┃ new int[n];   ┃      
+       ┗━━━━━━┛      ┗━━━━━━━━━━━━━━━┛
+
+
+       
+           */
+
+    return 0;
+}
