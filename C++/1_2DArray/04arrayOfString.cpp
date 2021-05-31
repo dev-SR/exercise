f8192bddafcbf1e3635d6894959439e093c28659
Soikat Rahman <hello.dev.sr@gmail.com>
2021-05-31 17:22:10 +0600

    C++: array of string

---
+++ C++/1_2DArray/04arrayOfString.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define RED "\e[0;91m"
+#define GRN "\e[0;92m"
+#define YEL "\e[0;93m"
+#define BLU "\e[0;94m"
+#define MAG "\e[0;95m"
+#define CYN "\e[0;96m"
+#define reset "\e[0m"
+
+int main() {
+    char ar[3][20] = {"Hello World", "Welcome", "Bye Bye"};
+    for (int i = 0; i < 3; i++) {
+        cout << ar[i] << endl;
+    }
+
+    int n;
+    cin >> n;
+    cin.ignore(); //to ignore the enter after n;
+
+    char a[10][100];
+    for (int i = 0; i < n; i++) {
+        cin.getline(a[i], 100);
+    }
+    for (int i = 0; i < n; i++) {
+        cout << a[i] << endl;
+    }
+
+    // Search
+    char key[100];
+    cout << CYN << "Enter string to search: " << reset << endl;
+    cin.getline(key, 100);
+    int i;
+    for (i = 0; i < n; i++) {
+        if (strcmp(key, a[i]) == 0) {
+            cout << "Found at:" << i + 1 << endl;
+            break;
+        }
+    }
+    if (i == n) {
+        cout << "Not found!";
+    }
+
+    return 0;
+}
