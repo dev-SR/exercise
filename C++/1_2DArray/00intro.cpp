8443bca6285584b8a1e91bba778edc5380ed7b4f
Soikat Rahman <hello.dev.sr@gmail.com>
2021-05-31 16:48:38 +0600

    C++: 2D array v2.1

--- C++/1_2DArray/00intro.cpp
+++ C++/1_2DArray/00intro.cpp
@@ -1,5 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define RED "\e[0;91m"
+#define GRN "\e[0;92m"
+#define YEL "\e[0;93m"
+#define BLU "\e[0;94m"
+#define MAG "\e[0;95m"
+#define CYN "\e[0;96m"
+#define reset "\e[0m"
+
 int main() {
 
     //2D arrays Deceleration/Initialization:
@@ -7,12 +15,37 @@ int main() {
     int ar2[2][3] = {{1, 2, 3}, {3, 4, 5}};
     int ar3[][3] = {{1, 2, 3}, {3, 4, 5}};
     char ar4[3][2] = {{'A', 'B'}, {'C', 'D'}, {'E', 'F'}};
-    char ar5[][4] = {"abc", "def", "efg", "hij"};
+    char ar5[][4] = {{'A', 'B', '\0'}, {'C', 'D', '\0'}, {'E', 'F', '\0'}};
+    char ar6[][4] = {"abc", "def", "efg", "hij"};
 
-    cout << ar3[0][0] << endl; //1
-    cout << ar3[0] << endl;    //0x61fdb0
-    cout << ar4[0][0] << endl; //A
-    cout << ar4[0] << endl;    //ABCDEF
+    cout << CYN << "ar3[0][0]: " << reset << ar3[0][0] << endl; //1
+    cout << CYN << "ar3[0]   : " << reset << ar3[0] << endl;    //0x61fdb0
+    cout << CYN << "ar4[0][0]: " << reset << ar4[0][0] << endl; //A
+    cout << CYN << "ar4[0]   : " << reset << ar4[0] << endl;    //ABCDEF
+    cout << CYN << "ar5[0]   : " << reset << ar5[0] << endl;    //AB
+    cout << CYN << "ar6[0]   : " << reset << ar6[0] << endl;    //abc
+
+    //So, Creating
+
+    /**
+    {{'A', 'B', '\0'}, {'C', 'D', '\0'}, {'E', 'F', '\0'}};
+       ┏━━━┓━━━┓━━━┓     
+       ┃ A ┃ B ┃\0 ┃
+       ┗━━━┛━━━┛━━━┛
+       ┏━━━┓━━━┓━━━┓     
+       ┃ C ┃ D ┃\0 ┃
+       ┗━━━┛━━━┛━━━┛     
+    {"abc", "def", "efg", "hij"};
+       ┏━━━━━┓
+       ┃ abc ┃
+       ┗━━━━━┛
+       ┏━━━━━┓
+       ┃ def ┃
+       ┗━━━━━┛
+       ┏━━━━━┓
+       ┃ efg ┃
+       ┗━━━━━┛
+    */
 
     int a[100][100] = {0};
     int m, n;
