4c0de3b12b360c64834d2065374280d34e9843bc
Soikat Rahman <hello.dev.sr@gmail.com>
2021-06-06 10:05:35 +0600

    C++: Shallow Copy

--- C++/4_OPP/01_more_constructors/03_ShallowAndDeep_Copy.cpp
+++ C++/4_OPP/01_more_constructors/03_ShallowAndDeep_Copy.cpp
@@ -8,7 +8,42 @@ using namespace std;
 #define CYN "\e[0;96m"
 #define reset "\e[0m"
 
+class StudentShallow {
+    char *name;
+
+public:
+    StudentShallow(char *name) {
+        this->name = name; // SHALLOW COPY
+    }
+    void display() {
+        cout << name << endl;
+    }
+};
 int main() {
 
+    //* SHALLOW COPY
+    char name[] = "soikat";
+    StudentShallow s(name);
+    cout << "s: " << setw(7);
+    s.display();
+
+    //
+    name[2] = 'o';
+    StudentShallow s1(name);
+    cout << "s1: " << setw(6);
+    s1.display();
+    cout << "s: " << setw(8);
+    cout << RED;
+    s.display();
+    cout << reset << endl;
+
+    // We can see that modifing 's1' modifies 's' object
+    // Beacause of shallow coping of 'name' variable when
+    // creating objects 's' and 's1'
+    /**
+    ┏━━━━━┓     ┏━━━━━━┓     ┏━━━━━┓
+    ┃ s1  ┃<----┃ name ┃---->┃ s1  ┃
+    ┗━━━━━┛     ┗━━━━━━┛     ┗━━━━━┛
+*/
     return 0;
 }
