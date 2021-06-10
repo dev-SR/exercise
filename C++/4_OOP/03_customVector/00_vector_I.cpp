03f14997ad2bef987af86b7b89c66559bca5fa80
Soikat Rahman <hello.dev.sr@gmail.com>
2021-06-10 14:19:22 +0600

    C++: custom vector class

---
+++ C++/4_OOP/03_customVector/00_vector_I.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define RED "\e[0;91m"
+#define GRN "\e[0;92m"
+#define YEL "\e[0;93m"
+#define BLU "\e[0;94m"
+#define MAG "\e[0;95m"
+#define CYN "\e[0;96m"
+#define reset "\e[0m"
+
+class Vector {
+    int *arr;
+    int cs; //current size
+    int maxSize;
+
+public:
+    Vector(int defaultSize = 4) {
+        maxSize = defaultSize;
+        cs = 0;
+        arr = new int[maxSize];
+    }
+};
+
+int main() {
+
+    return 0;
+}
