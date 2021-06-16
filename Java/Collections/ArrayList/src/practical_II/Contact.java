df0c4d9144c6318702393c2d1e5c53fa37bda189
Soikat Rahman <hello.dev.sr@gmail.com>
2021-06-17 01:50:36 +0600

    java: Arraylist Practical examples..

---
+++ Java/Collections/ArrayList/src/practical_II/Contact.java
@@ -0,0 +1,27 @@
+package practical_II;
+
+/**
+ * Created by dev on 28/08/15.
+ * Revised by Tim on 4th Feb 2018.
+ */
+public class Contact {
+    private String name;
+    private String phoneNumber;
+
+    public Contact(String name, String phoneNumber) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public static Contact createContact(String name, String phoneNumber) {
+        return new Contact(name, phoneNumber);
+    }
+}
