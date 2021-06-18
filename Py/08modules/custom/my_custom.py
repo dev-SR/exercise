82bebcbe164514350f27a40d3471a6eb8ecc7747
Soikat Rahman <hello.dev.sr@gmail.com>
2021-06-19 05:36:21 +0600

    Py: creating custom modules

---
+++ Py/08modules/custom/my_custom.py
@@ -0,0 +1,14 @@
+def greetings(name):
+    """greetings
+
+    Args:
+        name (string): Your name
+
+    Returns:
+        string: returns your name with greetings
+
+    NOTES: use 'generate docstring' commands to generate DocStr
+
+
+    """
+    return "Hello, " + name
