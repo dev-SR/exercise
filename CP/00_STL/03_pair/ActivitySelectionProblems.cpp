d4293571095fa76a88acdb97bcdde63172fb27d2
Soikat Rahman <hello.dev.sr@gmail.com>
2021-07-10 18:25:03 +0600

    STL: ActivitySelectionProblems fix

--- CP/00_STL/03_pair/ActivitySelectionProblems.cpp
+++ CP/00_STL/03_pair/ActivitySelectionProblems.cpp
@@ -6,7 +6,10 @@ Activity Selection Problems
 ! Select the maximum number of activities that can be performed by a single 
 ! person, assuming that a person can only work on a single activity at a time. 
 
-Input Format: The first line consists of an integer T, the number of test cases. For each test case,the first line consists of an integer N, the number of activities. Then the next N lines contain two integers m and n, the start and end time of each activity.
+Input Format: The first line consists of an integer T, the number of test 
+cases. For each test case,the first line consists of an integer N, the number
+of activities. Then the next N lines contain two integers m and n, the start 
+and end time of each activity.
 
 Sample Input: 
 1 
@@ -14,7 +17,10 @@ Sample Input:
 10 20
 12 15
 20 30
-Output Format: For each test case find the maximum number of activities that you can do.
+
+Output Format: For each test case find the maximum number of activities 
+that you can do.
+
 Sample Output: 2
 
 
