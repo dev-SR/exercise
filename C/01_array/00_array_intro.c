695e412c396ef46e2427dc804580df8b5ec8eacb
Soikat Rahman <hello.dev.sr@gmail.com>
2021-07-12 04:30:17 +0600

    C: Array basic

--- C/01_array/00_array_intro.c
+++ C/01_array/00_array_intro.c
@@ -16,22 +16,22 @@ int main() {
 
     printf(GRN "=== Initializing Arrays ===\n" NC);
 
-    // int a[LEN]; // a has type int[5] and contains unpredictable values!
+    int a[LEN]; // a has type int[5] and contains unpredictable values!
 
-    // /* Before moving on we need a to contain all 1s */
-    // for (int i = 0; i < LEN; i++) {
-    //     a[i] = 1;
-    // }
+    /* Before moving on we need a to contain all 1s */
+    for (int i = 0; i < LEN; i++) {
+        a[i] = 1;
+    }
 
-    // /* Array initialization from brace-enclosed lists */
-    // int x[] = {1, 2, 3, 4, 5}; //1, 2, 3, 4, 5
-    // int y[LEN] = {1, 2, 3};    //1, 2, 3, 0, 0
-    // int z[LEN] = {0};          //0, 0, 0, 0, 0
+    /* Array initialization from brace-enclosed lists */
+    int x[] = {1, 2, 3, 4, 5}; //1, 2, 3, 4, 5
+    int y[LEN] = {1, 2, 3};    //1, 2, 3, 0, 0
+    int z[LEN] = {0};          //0, 0, 0, 0, 0
 
-    // /* Array initialization with designators (sparse array) */
-    // int w[LEN] = {[1] = 1, [3] = 1};               //0, 1, 0, 1, 0
-    // int q[LEN] = {[1] = 3, [LEN - 1] = 5};         //0, 3, 0, 0, 5
-    // int t[10] = {[0] = 5, 4, [LEN - 3] = 3, 2, 1}; //5, 4, 0, 0, ..., 3, 2, 1
+    /* Array initialization with designators (sparse array) */
+    int w[LEN] = {[1] = 1, [3] = 1};               //0, 1, 0, 1, 0
+    int q[LEN] = {[1] = 3, [LEN - 1] = 5};         //0, 3, 0, 0, 5
+    int t[10] = {[0] = 5, 4, [LEN - 3] = 3, 2, 1}; //5, 4, 0, 0, ..., 3, 2, 1
 
     printf(GRN "=== Find the size of an Array ===\n" NC);
     //%ld , sizeof operator returns the output in size_t format.
