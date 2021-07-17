076d1b76fe1f88e90c45fc67017e33b852d8e148
Soikat Rahman <hello.dev.sr@gmail.com>
2021-07-17 21:24:21 +0600

    C++: C-style stirng tokenization

--- C++/0string/02_strtok_Tokenization.cpp
+++ C++/0string/02_strtok_Tokenization.cpp
@@ -1,6 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+#define RED "\e[0;91m"
+#define GRN "\e[0;92m"
+#define YEL "\e[0;93m"
+#define BLU "\e[0;94m"
+#define MAG "\e[0;95m"
+#define CYN "\e[0;96m"
+#define NC "\e[0m"
 /**
 USECASE://* spliting a string in C/C++
 //https://www.geeksforgeeks.org/how-to-split-a-string-in-cc-python-and-java/
@@ -22,66 +28,102 @@ SEE: 0_StringStream.cpp for more deatails
 
 *!  char* strtok( char* str, const char* delim );
 
-The strtok() function takes two arguments: str and delim. 
-This function finds the token in the string pointed to by strtok. 
-The pointer delim points to the separator characters.
+! `char *strtok(char *str, const char *delim)`
 
-*? This function can be called multiple times to obtain tokens from the same string. There are two cases:
+*?- str: The string which is to be split
+*?- delim: The character on the basis of which the split will be done
 
-*?If str is not NULL:---------
-A call to strtok() is considered first call for that string. 
-The function searches for the first character that is not contained in delim. 
-If no such character is found, the string does not contain any token. So a null pointer is returned. 
-If such character is found, from there on the function searches for a character that is present in delim. 
-If no separator is found, str has only one token. 
-If a separator is found, it is replaced by '\0' and 
-the pointer to the following character is stored in a static location for subsequent invocations. 
-Finally, the function returns the pointer to the beginning of the token.
+Return value:
+The function performs one split and returns a pointer to the token split up. A null pointer is returned if the string cannot be split.
 
-*?If str is NULL:------------
-The call is considered as subsequent calls to strtok and the function continues from where it
- left in previous invocation.
-It is defined in <cstring> header file.
+** To find all possible splits of the string, based on a given delimiter,
+** the function needs to be called in a loop.A sequence of calls to strtok()
+** that operate on the same string  maintains a pointer that determines the 
+** point from which to start  searching for the next token.
 
-*?strtok() Parameters
-**str: Pointer to the null terminated byte string to tokenize.
-**delim: Pointer to the null terminated byte string that contains the separators.
-
-*?strtok() Return value
-**The strtok() function returns the pointer to the next token if there is any,
-**or it returns NULL if no more tokens are found.
+** So,In first call->
+*! - strtok(str) 
+*?On the first call to strtok() the string to be parsed should be specified in str.
+** In each subsequent call->
+*! - strtok(NULL) 
+*?In each subsequent call that should parse the same string, str must be NULL.
+//https://man7.org/linux/man-pages/man3/strtok.3.html
 
 */
 
-int main() {
+int tokenize_string(char str[], const char delimiters[], int tok_max_len, char tokens[][100]);
+void print_tokens(int n_tokens, char tokens[][100], char str[], const char delimiters[]);
 
+int main() {
+    printf(CYN "1. Extracting a single token\n" NC);
+    char string[50] = "Hello world";
+    // Extract the first token
+    char *token = strtok(string, " ");
+    printf(" %s\n", token); //printing the token
+
+    printf(CYN "2. Extracting all possible tokens\n" NC);
     /**
  * So, strtok returns a token on each subsequent call
  * on the first call function should be passed with string argument for 's'
  * on subsequent calls we should pass the string argument as null
 */
-    char c[100] = "Today, is a rainy, day";
+    char s[50] = "Hello! We are learning about strtok";
+    // Extract the first token
+    char *tokens = strtok(s, " ");
+    // loop through the string to extract all other tokens
+    while (tokens != NULL) {
+        printf(" %s\n", tokens); //printing each token
+        tokens = strtok(NULL, " ");
+    }
+    printf("\n");
 
-    //Working With String
-    //>>>string ss = "Today, is a rainy, day";
-    //Converting String to Array of char.
-    //>>>char *s = (char *)ss.c_str();
-    //1. CASTING to solve: "const char *" cannot be used to initialize an entity of type "char *"
-    //2. The c_str() function is used to return a pointer to an array that contains a null
-    //   terminated sequence of character representing the current value of the string.
-    //>>>char *ptr = strtok(s, " ");
+    printf(CYN "3. Example\n" NC);
 
-    // *? If str is not NULL:---------
-    // A call to strtok() is considered first call for that string.
-    char *ptr = strtok(c, " "); //
-    cout << ptr << endl;
-    while (ptr != NULL) {
-        //  *? If str is NULL : ------------
-        //The call is considered as subsequent calls to strtok and
-        //the function continues from where it left in previous invocation.
-        ptr = strtok(NULL, " ");
-        cout << ptr << endl;
+    char str[] = "This, is a. sample-string";
+    const char delimiters[] = " ,.-";
+    int tok_max_len = sizeof(str);
+    char tok[100][100], str_cpy[tok_max_len];
+    strncpy(str_cpy, str, tok_max_len);
+
+    int n_tokens = tokenize_string(str_cpy, delimiters, tok_max_len, tok);
+    print_tokens(n_tokens, tok, str, delimiters);
+
+    return 0;
+}
+/* return the number of tokens found in the string */
+int tokenize_string(char str[], const char delimiters[], int tok_max_len, char tokens[][100]) {
+    int i;
+    char *ptok = str;
+    for (i = 0; ptok; i++) {
+        ptok = strtok(i == 0 ? ptok : NULL, delimiters);
+        strncpy(tokens[i], ptok, ptok ? tok_max_len : 0);
+    }
+    return i - 1;
+}
+
+void print_tokens(int n_tokens, char tokens[][100], char str[], const char delimiters[]) {
+    printf("Tokenization with delimiters: " GRN " \"%s\"\n" NC, delimiters);
+    printf("%s\n", str);
+    for (int i = 0; i < n_tokens; i++) {
+        printf("[%d] %s\n", i, tokens[i]);
     }
 }
 
-//
+/* 1. Extracting a single token
+ Hello
+2. Extracting all possible tokens
+ Hello!
+ We
+ are
+ learning
+ about
+ strtok
+
+3. Example
+Tokenization with delimiters:  " ,.-"
+This, is a. sample-string
+[0] This
+[1] is
+[2] a
+[3] sample
+[4] string */
