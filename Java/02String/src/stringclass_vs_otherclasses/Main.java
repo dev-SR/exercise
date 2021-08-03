92d2d1b0dcc7654b25981a4e60c83d80a155f1fe
Soikat Rahman <hello.dev.sr@gmail.com>
2021-08-03 20:29:30 +0600

    Java: diff time complexity

--- Java/02String/src/stringclass_vs_otherclasses/Main.java
+++ Java/02String/src/stringclass_vs_otherclasses/Main.java
@@ -7,6 +7,46 @@ public class Main {
     public static void main(String[] args) {
         Color.printStartWithMsg(20, Color.GREEN_BRIGHT, "Differences In Concatenation");
         DiffInConcat();
+        Color.printStartWithMsg(20, Color.GREEN_BRIGHT, "Time Differentials:");
+        DiffInTime();
+
+    }
+
+    private static void DiffInTime() {
+        long start = System.currentTimeMillis();
+        appendString(10000);
+        long end = System.currentTimeMillis();
+        Color.printMsg(Color.YELLOW_BRIGHT, "Time diff for new String() | O(n^2): " + (end - start) + " msec");
+        start = System.currentTimeMillis();
+        appendStringBuiler(10000);
+        end = System.currentTimeMillis();
+        Color.printMsg(Color.YELLOW_BRIGHT, "Time diff for new StringBuilder() | O(n): " + (end - start) + " msec");
+        start = System.currentTimeMillis();
+        appendStringBuffer(10000);
+        end = System.currentTimeMillis();
+        Color.printMsg(Color.YELLOW_BRIGHT, "Time diff for new StringBuffer() | O(n): " + (end - start) + " msec");
+
+    }
+
+    private static void appendStringBuffer(int n) {
+        StringBuilder s = new StringBuilder();
+        for (int i = 1; i <= n; i++) {
+            s.append(i);
+        }
+    }
+
+    private static void appendStringBuiler(int n) {
+        StringBuilder s = new StringBuilder();
+        for (int i = 1; i <= n; i++) {
+            s.append(i);
+        }
+    }
+
+    private static void appendString(int n) {
+        String s = "";
+        for (int i = 1; i <= n; i++) {
+            s = s + i;
+        }
     }
 
     public static void DiffInConcat() {
