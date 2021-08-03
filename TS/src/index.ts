6568bb4e657103336c2e4829e8ae6d781fa80f67
Soikat Rahman <hello.dev.sr@gmail.com>
2021-08-03 14:44:01 +0600

    TS: initial typescript setup

--- TS/src/index.ts
+++ TS/src/index.ts
@@ -1,18 +1,1 @@
-// import 'colors';
-// console.log('----------------<ts>---------------'.bgMagenta.black.bold);
-let dictionary: Record<string, States> = {};
-// type Dictionary ={[key:string]:States}
-interface States {
-   current: string;
-   next: string;
-}
-
-const Item: Record<keyof States, string> = {
-   current: 'ABC',
-   next: 'sodfhjk'
-};
-// type Items ={[key:'current'|'next']:string}
-
-dictionary[0] = Item;
-
-console.log(dictionary); //{ '0': { current: 'ABC', next: 'sodfhjk' } }
+console.log('Hello World');
