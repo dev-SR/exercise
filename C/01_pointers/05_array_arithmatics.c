8c7b66ec7922ed3a1f97f6ea0e52c70580c67718
Soikat Rahman <hello.dev.sr@gmail.com>
2021-10-09 10:14:38 +0600

    C: pointer arithmatic

--- C/01_pointers/05_array_arithmatics.c
+++ C/01_pointers/05_array_arithmatics.c
@@ -8,23 +8,6 @@
 #define NC "\e[0m"
 
 #define X_LEN 4
-
-int sum_arr(int arr[], const int len) {
-    int sum = 0;
-    for (int i = 0; i < len; i++) {
-        sum += arr[i];
-    }
-    return sum;
-}
-
-int sum_ptr(const int *const p, const int len) {
-    int sum = 0;
-    for (int i = 0; i < len; i++) {
-        sum += p[i];
-    }
-    return sum;
-}
-
 int sum_ptr_arithmetic(const int *const p, const int len) {
     printf(YEL "iterating with ptr++ \n" NC);
     int sum = 0;
@@ -33,25 +16,20 @@ int sum_ptr_arithmetic(const int *const p, const int len) {
         // for (; ;) {
         // p_item++
         sum += *p_item;
-        printf("%ld, %ld, %d\n", p_item - p, p_item, sum);
+        printf("%d, %X, %d\n", p_item - p, p_item, sum);
         p_item++;
     }
     return sum;
 }
-
 int main() {
     printf("\n=== Pointers Arithmetic ===\n\n");
 
     int x[X_LEN] = {1, 4, -3, 2};
-
-    printf("sum_arr: %d\n", sum_arr(x, X_LEN));
-    printf("sum_ptr: %d\n", sum_ptr(x, X_LEN));
-
-    printf(GRN "ptr++\n" NC);
     int *ptr = x; // same as: int *ptr = &x[0]
-    printf("%ld, %d\n", ptr, *ptr);
+    printf("%X, %d\n", ptr, *ptr);
+    printf(YEL "ptr++\n" NC);
     ptr++; // same as: ptr = ptr + 1
-    printf("%ld, %d\n", ptr, *ptr);
+    printf("%X, %d\n", ptr, *ptr);
 
     printf("---\n");
     printf("sum_ptr_arithmetic: %d\n", sum_ptr_arithmetic(x, X_LEN));
@@ -62,7 +40,7 @@ int main() {
      *    pointer / pointer
      *    pointer * pointer
      *    pointer % pointer
-     * 
+     *
      */
 
     return 0;
